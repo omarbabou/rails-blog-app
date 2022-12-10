7ad40c6d7e072fba85457838be9ec3ae751e2ba4
omarbabou <obabou50@gmail.com>
2022-12-10 14:17:46 +0200

    Correct errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,16 +2,16 @@ class ApplicationController < ActionController::Base
     def current_user
         User.find(params[:id] || params[:user_id])
     end
-    
+  
     def all_users
-        User.all.order('id asc')
+      User.all.order('id asc')
     end
-    
+  
     def current_post
-        User.find(params[:user_id]).posts.find(params[:id] || params[:post_id])
+      User.find(params[:user_id]).posts.find(params[:id] || params[:post_id])
     end
-    
+  
     def all_posts
-        User.find(params[:user_id])
+      User.find(params[:user_id])
     end    
 end

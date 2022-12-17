5831af621ad0cc82d3b1a12599b493deb9c9aa18
omarbabou <obabou50@gmail.com>
2022-12-17 12:58:19 +0200

    Add role to user by running rails migrate

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-  :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable, :confirmable
   has_many :posts, foreign_key: 'author_id'
   has_many :comments, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
@@ -12,5 +12,9 @@ class User < ApplicationRecord
                             numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   def recent_posts
     posts.order(created_at: :desc).limit(3)
+  end
+
+  def admin?
+    role == 'admin' ? true : false
   end
 end
